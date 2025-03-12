27366cc7959adb0e7a9c5adc26bd05b3bb28b744
alexa-Dev7 <trishnit@outlook.com>
2025-03-12 12:47:14 +0530

    server.cpp

--- server.cpp
+++ server.cpp
@@ -11,13 +11,25 @@ using namespace std;
 void loadMessages() {
     ifstream file("messages.json");
     json messages;
-    file >> messages;
+
+    // Check if file is empty or broken
+    if (file.peek() == ifstream::traits_type::eof()) {
+        messages = json::array();
+    } else {
+        file >> messages;
+    }
 
     system("clear");
     cout << "🔥 Chat Room 🔥\n\n";
 
-    for (auto& msg : messages) {
-        cout << msg["user"] << ": " << msg["text"] << endl;
+    if (messages.empty()) {
+        cout << "No messages yet. Start the conversation!\n";
+    } else {
+        for (auto& msg : messages) {
+            if (msg.contains("user") && msg.contains("text")) {
+                cout << msg["user"] << ": " << msg["text"] << endl;
+            }
+        }
     }
 }
 
